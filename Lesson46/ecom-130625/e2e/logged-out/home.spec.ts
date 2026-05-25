0d32e942505b48429fdc09fe29ed41cdd641256f
annasvst <annaatalay@hotmail.com>
2026-05-25 21:24:46 +0200

    Add correction for lesson 46 url

--- Lesson46/ecom-130625/e2e/logged-out/home.spec.ts
+++ Lesson46/ecom-130625/e2e/logged-out/home.spec.ts
@@ -4,8 +4,8 @@ test('Logged out user should see Login button on the home page', async ({ page }
   await page.goto('/');
   await page.getByRole('link', { name: 'Login' }).click();
 
-  // Anna to fix regex for the URL
-  //await expect(page).toHaveURL('https://dev-jm5m0z3xkqxztipx.us.auth0.com/u/login?**');
+  // Correct regext for the URL
+  await expect(page).toHaveURL(/dev-jm5m0z3xkqxztipx\.us\.auth0\.com\/u\/login/);
 });
 
 test('Logged out user should see 8 products on the home page', async ({ page }) => {
